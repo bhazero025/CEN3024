dfc3e143e94ed52b4402eade86a8d9899899b757
Bruno Harger Alves <bhazero025@gmail.com>
2018-02-01 15:24:15 -0500

    Update InvalidLocationException.java

--- week04/InvalidLocationException.java
+++ week04/InvalidLocationException.java
@@ -1,5 +1,21 @@
 package week04;
 
-public class InvalidLocationException extends Exception {
-
+public class InvalidLocationException extends Exception 
+{
+	private Location location;
+	public InvalidLocationException()
+	{
+		super();
+	}
+	
+	public InvalidLocationException(Location l) 
+	{
+		super("Invalid Location " + l.getAvenue() + ", " + l.getStreet());
+		location = l;
+	}
+	
+	public Location getInvalidLocation()
+	{
+		return location;
+	}
 }
