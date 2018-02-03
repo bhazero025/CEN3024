7fdb4d7a056c79381f8903ffaf5dbb1a6870dd5d
Dylan <Dyansh6594@yahoo.com>
2018-02-02 23:04:06 -0500

    Comments

--- week04/GasStation.java
+++ week04/GasStation.java
@@ -2,26 +2,32 @@ package week04;
 
 public class GasStation {
 
+	//variables
 	private String name;
 	private Location location;
 	
+	//constructor
 	public GasStation(String name, Location location) {
 		this.name = name;
 		this.location = location;
 	}
 
+	//returns name of the gas station
 	public String getName() {
 		return name;
 	}
 
+	//sets name of the gas station
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	//returns location of gas station
 	public Location getLocation() {
 		return location;
 	}
 
+	//sets location of the gas station
 	public void setLocation(Location location) {
 		this.location = location;
 	}
